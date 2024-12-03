8d69edd49f5b6b0c23ff91fd18df555197d129e0
PC-FIXE-HUGO\Hugo <hugotempezpro@gmail.com>
2024-12-03 23:53:54 +0100

    EntityManager et migration (une par éxecution) OK, checker les TODO pour les bugs

--- src/main/java/fr/itii25/projet_evaluation/Main.java
+++ src/main/java/fr/itii25/projet_evaluation/Main.java
@@ -7,8 +7,11 @@ import fr.itii25.tasks.ThreadRecepteur;
 //TODO: Si table vidé, pas de réinsertion avant redémarrage de l'appli
 //TODO: Si migration de toute les tables, uniquement la première est effectué
 //TODO: Une seul migration par éxecution de l'appli
+//TODO: Migrations all ok sauf film_actor et film_category (nom table ?)
+//TODO: film_text ?
 public class Main {
     public static void main(String[] args) {
+        //TODO: Faire menu ici et créer threads à la volée ?
         ThreadEmetteur emetteur = new ThreadEmetteur();
         ThreadRecepteur recepteur = new ThreadRecepteur();
 
