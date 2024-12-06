02729d256ed138f43f130af9183599cacaa2c081
Lucas.LEFEVRE <lucas.lefevre60@gmail.com>
2024-12-06 12:10:44 +0100

    Factory Connexion DB

--- src/main/java/fr/itii25/tasks/Task.java
+++ src/main/java/fr/itii25/tasks/Task.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Interface commune au threads émetteurs et récepteurs
+ * Interface commune aux threads émetteurs et récepteurs
  */
 public abstract class Task extends Thread {
 
@@ -20,7 +20,7 @@ public abstract class Task extends Thread {
      * @return le DAO corréspondant à la class demandé
      */
     public final DAO<?> getDao(Class<?> persistentClass, String persistenceUnitName) {
-        // Cherche le DAO correspondant à la classe peristante, si il n'existe pas, il est créé, ajouté à la liste, puis retourné
+        // Cherche le DAO correspondant à la classe peristante, s'il n'existe pas, il est créé, ajouté à la liste, puis retourné
         return daos.computeIfAbsent(persistentClass,  _ -> DAO.of(persistentClass, persistenceUnitName));
     }
 
